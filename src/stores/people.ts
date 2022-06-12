ea2c332a1370570cadef849e4aab84d9594acc7d
Artis Vansovics <vansovics.artis@gmail.com>
2022-06-13 02:05:06 +0300

    Created baseURL for people data requests with axios.create. Added a query parameter to getPeople function

--- src/stores/people.ts
+++ src/stores/people.ts
@@ -2,6 +2,10 @@ import { defineStore } from "pinia";
 import axios from "axios";
 import type { PeopleData } from "@/models/PeopleModel";
 
+const swapiPeople = axios.create({
+  baseURL: "https://swapi.dev/api/people",
+});
+
 interface PeopleState {
   peopleData: PeopleData;
   isLoading: boolean;
@@ -17,13 +21,13 @@ export const usePeopleStore = defineStore({
   }),
   getters: {},
   actions: {
-    async getPeople() {
+    async getPeople(query: string) {
       this.peopleData = {} as PeopleData;
       this.isLoading = true;
 
       try {
-        this.peopleData = await axios
-          .get<PeopleData>("https://swapi.dev/api/people")
+        this.peopleData = await swapiPeople
+          .get<PeopleData>(`/?search=${query}`)
           .then((response) => response.data);
       } catch (error) {
         this.error = error;
@@ -33,3 +37,23 @@ export const usePeopleStore = defineStore({
     },
   },
 });
+
+// actions: {
+//   setNameQuery(query: string) {
+//     this.nameQuery = query;
+//   },
+//   async getPeople() {
+//     this.peopleData = {} as PeopleData;
+//     this.isLoading = true;
+//
+//     try {
+//       this.peopleData = await swapiPeople
+//         .get<PeopleData>(`/?search=${this.nameQuery}`)
+//         .then((response) => response.data);
+//     } catch (error) {
+//       this.error = error;
+//     } finally {
+//       this.isLoading = false;
+//     }
+//   },
+// },
