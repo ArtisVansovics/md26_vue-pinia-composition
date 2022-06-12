1aa0f48f5fe56aad6b3a9893691d4247d5d0040f
Artis Vansovics <vansovics.artis@gmail.com>
2022-06-13 01:15:51 +0300

    PeopleView now displays results from people api data request

--- src/views/PeopleView.vue
+++ src/views/PeopleView.vue
@@ -1,12 +1,30 @@
 <template>
   <h1>People</h1>
+  <p v-if="isLoading">Loading...</p>
+  <p v-if="error">{{ error.message }}</p>
+  <p v-for="{ name, birth_year } in people" :key="name">
+    {{ name }} {{ birth_year }}
+  </p>
 </template>
 
 <script lang="ts">
-import { defineComponent } from "vue";
+import { computed, defineComponent } from "vue";
+import { usePeopleStore } from "@/stores/people";
+import { storeToRefs } from "pinia";
 
 export default defineComponent({
   name: "PeopleView",
+  setup() {
+    const { peopleData, isLoading, error } = storeToRefs(usePeopleStore());
+    const { getPeople } = usePeopleStore();
+
+    const people = computed(() => {
+      return peopleData.value.results;
+    });
+
+    getPeople();
+    return { peopleData, isLoading, people, error };
+  },
 });
 </script>
 
