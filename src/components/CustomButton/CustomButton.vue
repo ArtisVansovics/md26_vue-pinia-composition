963f20bdc18a36106480dfa8972cdf24c27315c4
Artis Vansovics <vansovics.artis@gmail.com>
2022-06-13 17:52:58 +0300

    Created CustomButton component

---
+++ src/components/CustomButton/CustomButton.vue
@@ -0,0 +1,22 @@
+<template>
+  <button class="button" @click="clickHandler" :disabled="isDisabled">
+    <slot></slot>
+  </button>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "CustomButton",
+  props: ["isDisabled"],
+  emits: ["onClick"],
+  methods: {
+    clickHandler() {
+      this.$emit("onClick");
+    },
+  },
+});
+</script>
+
+<style scoped lang="scss" src="./CustomButton.scss" />
