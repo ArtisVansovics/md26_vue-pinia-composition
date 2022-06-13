e52e96184f6cf95920f92d545333c3cf962f2717
Artis Vansovics <vansovics.artis@gmail.com>
2022-06-13 14:05:19 +0300

    Created FilmCard to show info on each film

---
+++ src/components/FilmCard/FilmCard.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="card">
+    <h2 class="title"><slot></slot></h2>
+    <h3 class="sub-title">Episode <slot name="episodeId"></slot></h3>
+    <p>Director: <slot name="director"></slot></p>
+    <p>Producer: <slot name="producer"></slot></p>
+    <p>Release Date: <slot name="releaseDate"></slot></p>
+    <p class="crawl"><slot name="openingCrawl"></slot></p>
+  </div>
+</template>
+
+<script>
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "FilmCard",
+});
+</script>
+
+<style scoped lang="scss" src="./FilmCard.scss" />
