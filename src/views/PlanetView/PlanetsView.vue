b5da9d11680d70c0dc581fbba87cd7d24a85ae6b
Artis Vansovics <vansovics.artis@gmail.com>
2022-06-13 17:53:37 +0300

    Added PlanetCard to PlanetsView to display info on planets

--- src/views/PlanetView/PlanetsView.vue
+++ src/views/PlanetView/PlanetsView.vue
@@ -2,16 +2,30 @@
   <h1>Planets</h1>
   <p v-if="isLoading">Loading...</p>
   <p v-if="error">{{ error.message }}</p>
-  {{ planets }}
+  <div v-if="planets" class="grid">
+    <PlanetCard
+      v-for="{ name, climate, terrain, population } in planets"
+      :key="name"
+    >
+      {{ name }}
+      <template #climate>{{ climate }}</template>
+      <template #terrain>{{ terrain }}</template>
+      <template #population>{{ population }}</template>
+      <template #button><CustomButton>Go to planet</CustomButton></template>
+    </PlanetCard>
+  </div>
 </template>
 
 <script lang="ts">
 import { computed, defineComponent } from "vue";
 import { storeToRefs } from "pinia";
 import { usePlanetStore } from "@/stores/planets";
+import PlanetCard from "@/components/PlanetCard/PlanetCard.vue";
+import CustomButton from "@/components/CustomButton/CustomButton.vue";
 
 export default defineComponent({
   name: "PlanetsView",
+  components: { CustomButton, PlanetCard },
   setup() {
     const { isLoading, error, planetData } = storeToRefs(usePlanetStore());
     const { getPlanets } = usePlanetStore();
