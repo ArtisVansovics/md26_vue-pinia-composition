6714b9e2c629808eab5de83473a008270e7da68f
Artis Vansovics <vansovics.artis@gmail.com>
2022-06-13 17:52:38 +0300

    Created PlanetCard component to show data on planets

---
+++ src/components/PlanetCard/PlanetCard.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="card">
+    <h2 class="name"><slot></slot></h2>
+    <p>Climate: <slot name="climate"></slot></p>
+    <p>Terrain: <slot name="terrain"></slot></p>
+    <p>Population: <slot name="population"></slot></p>
+    <slot name="button"></slot>
+  </div>
+</template>
+
+<script>
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "PlanetCard",
+});
+</script>
+
+<style scoped lang="scss" src="./PlanetCard.scss" />
