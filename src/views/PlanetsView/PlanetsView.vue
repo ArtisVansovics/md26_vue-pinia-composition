ccea99d37f904e23e05da8f50f4a70abd44e0185
Artis Vansovics <vansovics.artis@gmail.com>
2022-06-17 01:21:15 +0300

    Clicking on the 'Go to planet' button on Planet Card takes user to the specific planet's page

--- src/views/PlanetsView/PlanetsView.vue
+++ src/views/PlanetsView/PlanetsView.vue
@@ -4,14 +4,18 @@
   <p v-if="error">{{ error.message }}</p>
   <div v-if="planets" class="grid">
     <PlanetCard
-      v-for="{ name, climate, terrain, population } in planets"
+      v-for="{ name, climate, terrain, population, url } in planets"
       :key="name"
     >
       {{ name }}
       <template #climate>{{ climate }}</template>
       <template #terrain>{{ terrain }}</template>
       <template #population>{{ population }}</template>
-      <template #button><CustomButton>Go to planet</CustomButton></template>
+      <template #button>
+        <CustomButton @click="$router.push(`/planets/${getId(url)}`)">
+          Go to planet
+        </CustomButton>
+      </template>
     </PlanetCard>
   </div>
 </template>
@@ -34,9 +38,13 @@ export default defineComponent({
       return planetData.value.results;
     });
 
+    function getId(url: string) {
+      return url.split("/")[url.split("/").length - 2];
+    }
+
     getPlanets();
 
-    return { isLoading, error, planets };
+    return { isLoading, error, planets, getId };
   },
 });
 </script>
