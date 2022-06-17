bea1ee18f309a6ef401d1a698dd9a2ce5b2953ad
Artis Vansovics <vansovics.artis@gmail.com>
2022-06-17 13:59:13 +0300

    PlanetView now shows information on the planet

--- src/views/PlanetView/PlanetView.vue
+++ src/views/PlanetView/PlanetView.vue
@@ -1,7 +1,51 @@
 <template>
   <p v-if="isLoading">Loading...</p>
   <p v-if="error">{{ error.message }}</p>
-  <h1>{{ planetData.name }}</h1>
+  <h1 class="title">{{ planetData.name }}</h1>
+  <custom-button>View image</custom-button>
+  <p class="text">Population: {{ planetData.population }}</p>
+  <div class="grid">
+    <div class="column">
+      <h3>Planetary data</h3>
+      <p class="column__text">
+        Rotation period: {{ planetData.rotation_period }} hours
+      </p>
+      <p class="column__text">
+        Orbital period: {{ planetData.orbital_period }} standard days
+      </p>
+      <p class="column__text">Diameter: {{ planetData.diameter }} m</p>
+    </div>
+    <div class="column">
+      <h3>Environmental data</h3>
+      <p class="column__text">Climate conditions: {{ planetData.climate }}</p>
+      <p class="column__text">Terrain type: {{ planetData.terrain }}</p>
+      <p class="column__text">
+        Amount of surface water: {{ planetData.surface_water }}%
+      </p>
+    </div>
+  </div>
+  <h2>Known residents</h2>
+  <div class="grid small-gap">
+    <a
+      v-for="url in planetData.residents"
+      :href="url"
+      v-bind:key="url"
+      class="link"
+    >
+      {{ url }}
+    </a>
+  </div>
+  <h2>Films {{ planetData.name }} appears in</h2>
+  <div class="grid small-gap">
+    <a
+      v-for="url in planetData.films"
+      :href="url"
+      v-bind:key="url"
+      class="link"
+    >
+      {{ url }}
+    </a>
+  </div>
 </template>
 
 <script lang="ts">
@@ -9,9 +53,11 @@ import { defineComponent, onMounted } from "vue";
 import { storeToRefs } from "pinia";
 import { usePlanetStore } from "@/stores/planet";
 import { useRoute } from "vue-router";
+import CustomButton from "@/components/CustomButton/CustomButton.vue";
 
 export default defineComponent({
   name: "PlanetView",
+  components: { CustomButton },
   setup() {
     const { isLoading, error, planetData } = storeToRefs(usePlanetStore());
     const { getPlanet } = usePlanetStore();
