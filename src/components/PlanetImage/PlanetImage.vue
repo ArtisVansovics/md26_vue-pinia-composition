16420ba1df2a9f2eb960484d311f07d7d4aff14e
Artis Vansovics <vansovics.artis@gmail.com>
2022-06-19 22:38:00 +0300

    PlanetImage background changes based on planet terrain type

--- src/components/PlanetImage/PlanetImage.vue
+++ src/components/PlanetImage/PlanetImage.vue
@@ -25,8 +25,43 @@ export default defineComponent({
 
       this.planetTerrain === "desert"
         ? (color = "#F29F05")
-        : this.planetTerrain === "ocean"
+        : this.planetTerrain === "ocean" ||
+          this.planetTerrain === "oceans" ||
+          this.planetTerrain === "lakes"
         ? (color = "#0367A6")
+        : this.planetTerrain === "grasslands" ||
+          this.planetTerrain === "grassy hills" ||
+          this.planetTerrain === "plains"
+        ? (color = "#BFA617")
+        : this.planetTerrain === "jungle" ||
+          this.planetTerrain === "jungles" ||
+          this.planetTerrain === "rainforests"
+        ? (color = "#569638")
+        : this.planetTerrain === "swamp" || this.planetTerrain === "swamps"
+        ? (color = "#8C7C3B")
+        : this.planetTerrain === "gas giant"
+        ? (color = "#DB6707")
+        : this.planetTerrain === "forests"
+        ? (color = "#2ABF5B")
+        : this.planetTerrain === "cityscape" || this.planetTerrain === "urban"
+        ? (color = "#B3AFA8")
+        : this.planetTerrain === "rock" ||
+          this.planetTerrain === "rocky islands"
+        ? (color = "#734502")
+        : this.planetTerrain === "scrublands"
+        ? (color = "#D9C471")
+        : this.planetTerrain === "volcanoes"
+        ? (color = "#59110B")
+        : this.planetTerrain === "glaciers" ||
+          this.planetTerrain === "mountains" ||
+          this.planetTerrain === "tundra"
+        ? (color = "#A1BEBF")
+        : this.planetTerrain === "fungus forests"
+        ? (color = "#8E8BA6")
+        : this.planetTerrain === "caves" || this.planetTerrain === "barren"
+        ? (color = "#73706C")
+        : this.planetTerrain === "toxic cloudsea"
+        ? (color = "#5BF21D")
         : null;
 
       return color;
